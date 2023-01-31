6cccdde13365ca3f794266c91d95385d05f2c738
Nelson Escudero <nescudero@quad.com>
2023-01-31 10:47:46 -0400

    Add postgres config files for nestjs and sequelize

---
+++ memo-backend/src/config/db.migrations.js
@@ -0,0 +1,28 @@
+const configs = {
+    development: {
+        dialect: 'postgres',
+        database: process.env.POSTGRES_DB_NAME || 'dev_memo_game',
+        username: process.env.POSTGRES_DB_USERNAME || 'postgres',
+        password: process.env.POSTGRES_DB_PASSWORD || 'postgres',
+        host: process.env.POSTGRES_DB_HOST || 'localhost',
+        port: parseInt(process.env.POSTGRES_DB_PORT || '5455'),
+    },
+    test: {
+        dialect: 'postgres',
+        database: process.env.POSTGRES_DB_NAME || 'test_memo_game',
+        username: process.env.POSTGRES_DB_USERNAME || 'postgres',
+        password: process.env.POSTGRES_DB_PASSWORD || 'postgres',
+        host: process.env.POSTGRES_DB_HOST || 'localhost',
+        port: parseInt(process.env.POSTGRES_DB_PORT || '5432'),
+    },
+    production: {
+        dialect: 'postgres',
+        database: process.env.POSTGRES_DB_NAME,
+        username: process.env.POSTGRES_DB_USERNAME,
+        password: process.env.POSTGRES_DB_PASSWORD,
+        host: process.env.POSTGRES_DB_HOST,
+        port: parseInt(process.env.POSTGRES_DB_PORT),
+    },
+};
+
+module.exports = configs;
