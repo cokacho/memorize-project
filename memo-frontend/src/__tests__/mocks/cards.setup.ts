a9813ebe494304a874a3c7ebc874faa89877852b
Nelson Escudero <nescudero@quad.com>
2023-02-06 16:31:56 -0400

    Add unit tests for store redux and home page

---
+++ memo-frontend/src/__tests__/mocks/cards.setup.ts
@@ -0,0 +1,50 @@
+export const DefaultCards = [
+  {
+    id: 1,
+    type: '1',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 2,
+    type: '2',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 3,
+    type: '3',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 4,
+    type: '4',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 5,
+    type: '5',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 6,
+    type: '6',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 7,
+    type: '7',
+    image: '',
+    isTurnOver: false
+  },
+  {
+    id: 8,
+    type: '8',
+    image: '',
+    isTurnOver: false
+  }
+];
