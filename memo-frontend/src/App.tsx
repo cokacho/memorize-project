08f111819aeeb77fdc948cc6fece6c79fcdf6571
Nelson Escudero <nescudero@quad.com>
2023-02-06 15:44:44 -0400

    Add routes and navigation bar and css rules

--- memo-frontend/src/App.tsx
+++ memo-frontend/src/App.tsx
@@ -1,24 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/topbar/Topbar";
+import Home from "./components/home/Home";
+import Game from "./components/game/Game";
+import About from "./components/about/About";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Navbar />} >
+          <Route path={'/'} element={<Home />}/>
+          <Route path={'/about'} element={<About />}/>
+        </Route>
+        <Route path={'/game'} element={<Game />}/>
+      </Routes>
     </div>
   );
 }
