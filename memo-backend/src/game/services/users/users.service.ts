dd534294c44c9994d8d767a9b091428fa93dcab4
Nelson Escudero <nescudero@quad.com>
2023-02-06 13:10:14 -0400

    Add register controller and service logic

--- memo-backend/src/game/services/users/users.service.ts
+++ memo-backend/src/game/services/users/users.service.ts
@@ -10,10 +10,16 @@ export class UsersService {
     private readonly userModel: typeof User,
   ) {}
 
+
   create(createUserDto: CreateUserDto): Promise<User> {
     const body = JSON.stringify(createUserDto);
 
     return this.userModel.create(JSON.parse(body));
+  }
+
+  update(user): Promise<[User, boolean]> {
+
+    return this.userModel.upsert(user);
   }
 
   async findAll(): Promise<User[]> {
@@ -26,6 +32,16 @@ export class UsersService {
         id,
       },
     });
+  }
+
+  findOneByEmail(email: string): Promise<User> {
+    return this.userModel.findOne(
+      {
+        where: {
+          email,
+        }
+      }
+    )
   }
 
   async remove(id: string): Promise<void> {
